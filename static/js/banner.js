0adf3006efea74fefa80c68f445edd84334881c3
Sebastian <sebbecker@gmx.net>
2019-01-27 15:54:23 +0000

    add hub links to navbar

--- static/js/banner.js
+++ static/js/banner.js
@@ -1,5 +1,11 @@
-var burger = document.getElementById('menu_burger')
-var dropdown = document.getElementById('dropdown')
+var burger = document.getElementById('menu-burger')
+var dropdown = document.getElementById('navbar')
 burger.addEventListener('click', function() {
-  dropdown.style.display = (dropdown.style.display == 'block') ? 'none' : 'block';
+  navbar.style.display = (navbar.style.display == 'block') ? 'none' : 'block';
+})
+
+var more_btn = document.getElementById('more-btn')
+var more_menu = document.getElementById('more-menu')
+more_btn.addEventListener('click', function() {
+  more_menu.style.display = (more_menu.style.display == 'block') ? 'none' : 'block';
 })
