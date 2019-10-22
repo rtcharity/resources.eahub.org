de10799b381dd7c497afe140b600a5219dcc847f
Sebastian Becker <sebbecker@gmx.net>
2019-10-22 13:25:39 +0100

    Add success and error messages

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,16 +1,23 @@
 function submitForm() {
   var form = $('#form');
+  var errorMsg = $('#contact-error');
+  var successMsg = $('#contact-success');
   var updatedForm = "form-name=" + form.attr('name') + "&" + form.serialize();
 
   $.ajax({
-    url: "https://confident-minsky-f713d6.netlify.com/contact-lean/",
+    url: "https://confident-minsky-f713d6.netlify.com/contact/",
     method: "POST",
     headers: {
         "content-type": "application/x-www-form-urlencoded"
     },
-    data: "not what is needed",
+    data: updatedForm,
     success: function(result) {
-      console.log(result)
+      form.css('display', 'none');
+      successMsg.css('display', 'block');
+    },
+    error: function(err) {
+      form.css('display', 'none');
+      errorMsg.css('display', 'block');
     }
   });
 }
